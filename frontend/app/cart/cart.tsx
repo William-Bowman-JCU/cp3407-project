3aa1ea1abe893ea75564001c531683cd2014ae2e
JoeJCU <joedavmat12@hotmail.com>
2026-04-01 05:52:50 +1000

    started cart page

---
+++ frontend/app/cart/cart.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from 'react';
+
+export type cartItem = {
+    id: number;
+    name: string;
+    quantity: number;
+    price: number;
+}
+
+
+export default function cartPage() {
+    const [isLoading, setIsLoading] = React.useState(true);
+
+  return (
+    
+  );
+}
+
+
+//room for nav and footer
