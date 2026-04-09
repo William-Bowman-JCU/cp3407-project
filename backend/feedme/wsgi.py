4ef3b78ac83762bc94a51242e94932e765ec89d5
William Bowman <william.bowman@my.jcu.edu.au>
2026-04-09 15:06:26 +1000

    feat: refactored backend to use 'feedme' as the main application name, modified file structure to clear unnecessary files & migrated DB to AWS Aurora & tested connectivity

--- backend/feedme/wsgi.py
+++ backend/feedme/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfood.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedme.settings')
 
 application = get_wsgi_application()
