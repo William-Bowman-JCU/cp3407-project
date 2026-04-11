888c7ba14ced6e1f74c0c0c356274995b0abe301
JoeJCU <joedavmat12@hotmail.com>
2026-04-11 23:54:22 +1000

    More order hsitory page

--- frontend/app/orders/orderHistoryPage.tsx
+++ frontend/app/orders/orderHistoryPage.tsx
@@ -21,12 +21,33 @@ export default function OrderHistoryPage() {
     const router = useRouter();
     const  [orders, setOrders] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
-    useEffect() => {
+    useEffect(() => {
         // Fetch Data
-        try {
-            
-        }
+    }, []);
+
+    return (
+        <div className='min-h-screen text-white'>
+            <main className='max-w-3xl mx-auto px-6 py-10'>
+                <h1 className='text-3xl font-bold text-center mb-10 tracking-wise'>
+                    Your Order History:   
+                    {/* Could/need to replace 'Your' with the users logged in username or name */}
+                </h1>
+
+                {isLoading && (
+                    <div className='text-center text-zinc-400 py-20'>Loading Orders...</div>
+                )}
+
+                
+
+
+
+
+            </main>
+        </div>
+
+        );
     }
-}
+
 
---
+++ frontend/app/orders/page.tsx
@@ -0,0 +1,5 @@
+import OrderHistoryPage from "./orderHistoryPage";
+
+export default function page(){ //defines the
+    return <OrderHistoryPage />; 
+}
